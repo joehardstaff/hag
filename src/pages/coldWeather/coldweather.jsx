a6a4435836145e5a2f13d0c48d29939196534ffb
Joe Hardstaff <Joe.hardstaff@outlook.com>
2024-01-19 11:49:40 +0000

    fixing weather

--- src/pages/coldWeather/coldweather.jsx
+++ src/pages/coldWeather/coldweather.jsx
@@ -60,47 +60,44 @@ export function Coldweather(props) {
 
   const container = window !== undefined ? () => window().document.body : undefined;
 
-  const drawer = (
-    <div>
-      <Drawer
-        container={container}
-        variant="temporary"
-        open={mobileOpen}
-        onClose={handleDrawerToggle}
-        ModalProps={{
-          keepMounted: true,
-        }}
-        sx={{
-          display: {
-            xs: 'block',
-            sm: 'none',
-          },
-          '& .MuiDrawer-paper': {
-            boxSizing: 'border-box',
-            width: drawerWidth,
-            backgroundColor: '#2B3336',
-          },
-        }}
-      >
-        <Toolbar />
-        <Divider />
-        <List sx={{ backgroundColor: '#2B3336' }}>
-          {[
-            { text: 'Profile', icon: <InboxIcon sx={{ color: 'white' }} />, path: '/profile' },
-            { text: 'Air Quality', icon: <MailIcon sx={{ color: 'white' }} />, path: '/air-quality' },
-            { text: 'Hot Weather', icon: <InboxIcon sx={{ color: 'white' }} />, path: '/hot-weather' },
-            { text: 'Cold Weather', icon: <MailIcon sx={{ color: 'white' }} />, path: '/cold-weather' },
-            { text: 'Rainy Weather', icon: <InboxIcon sx={{ color: 'white' }} />, path: '/rainy-weather' },
-          ].map((item) => (
-            <ListItem key={item.text} disablePadding sx={{ backgroundColor: '#2B3336' }}>
-              <ListItemButton component={RouterLink} to={item.path}>
-                <ListItemIcon>{item.icon}</ListItemIcon>
-                <ListItemText primary={item.text} style={{ color: 'white' }} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
+
+    const drawer = (
+      <div>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: {
+              xs: 'none',
+              sm: 'block',
+            },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              bgcolor: '#2B3336',
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          {/* Drawer content */}
+          <Toolbar />
+          <Divider />
+          <List>
+            {[
+              { text: 'Profile', icon: <InboxIcon sx={{ color: 'white' }} />, path: '/profile' },
+              { text: 'Air Quality', icon: <MailIcon sx={{ color: 'white' }} />, path: '/air-quality' },
+              { text: 'Hot Weather', icon: <InboxIcon sx={{ color: 'white' }} />, path: '/hot-weather' },
+              { text: 'Cold Weather', icon: <MailIcon sx={{ color: 'white' }} />, path: '/cold-weather' },
+              { text: 'Rainy Weather', icon: <InboxIcon sx={{ color: 'white' }} />, path: '/rainy-weather' },
+            ].map((item) => (
+              <ListItem key={item.text} disablePadding>
+                <ListItemButton component={RouterLink} to={item.path}>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.text} style={{ color: 'white' }} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
     </div>
   );
 
